1fe5560e94f43dca31d210e6dd1059a459c3a4d2
Claudio OLiveira <dev.caon@gmail.com>
2025-02-19 19:57:27 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const HomePage = () => {
   return (
     <div className="p-5 text-red-500">
-      <h1 className=" font-bold italic">Hello, Giovanna Farias</h1>
+      <h1 className="font-bold italic">Hello World!</h1>
       <Button>FSW 7.0</Button>
     </div>
   );
