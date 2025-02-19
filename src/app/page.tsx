63ea6a050a67a21418eb50b1e29221155245ae0b
Claudio OLiveira <dev.caon@gmail.com>
2025-02-18 23:21:53 -0300

    chore: update css variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ const HomePage = () => {
   return (
     <div className="p-5 text-red-500">
       <h1 className=" font-bold italic">Hello, Giovanna Farias</h1>
-      <p className="text-xs">Gata gostosa</p>
       <Button>FSW 7.0</Button>
     </div>
   );
