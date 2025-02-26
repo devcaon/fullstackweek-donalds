1140998c79bf793f10c59daad46641488ddd9480
Claudio OLiveira <dev.caon@gmail.com>
2025-02-25 21:30:47 -0300

    feat: add product details page

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -3,34 +3,46 @@ import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
+import { formatCurrency } from "@/helpers/format-currency";
+
 interface ProductsProps {
-    products: Product[];
+  products: Product[];
 }
 
-const Products = ({products}: ProductsProps) => {
-    const { slug } = useParams<{ slug: string }>();
-    return (
-        <div className="space-y-3 px-5">
-            {products.map(product => (
-                <Link key={product.id} href={`/${slug}/menu/${product.id}`} className="flex items-center justify-between gap-10 py-3 border-b">
-                    {/* left */}
-                    <div>
-                        <h3 className="text-sm font-medium">{product.name}</h3>
-                        <p className="line-clamp-2 text-sm text-muted-foreground">{product.description}</p>
-                        <p className="pt-3 font-semibold text-sm">{new Intl.NumberFormat("pt-BR", {
-                            style: 'currency',
-                            currency: 'BRL'
-                        }).format(product.price)}</p>
-                    </div>
+const Products = ({ products }: ProductsProps) => {
+  const { slug } = useParams<{ slug: string }>();
+  return (
+    <div className="space-y-3 px-5">
+      {products.map((product) => (
+        <Link
+          key={product.id}
+          href={`/${slug}/menu/${product.id}`}
+          className="flex items-center justify-between gap-10 border-b py-3"
+        >
+          {/* left */}
+          <div>
+            <h3 className="text-sm font-medium">{product.name}</h3>
+            <p className="line-clamp-2 text-sm text-muted-foreground">
+              {product.description}
+            </p>
+            <p className="pt-3 text-sm font-semibold">
+              {formatCurrency(product.price)}
+            </p>
+          </div>
 
-                    {/* right */}
-                    <div className="relative min-h-[82px] min-w-[120px]">
-                        <Image src={product.imageUrl} alt={product.name} fill className="rounded-lg object-contain" />
-                    </div>
-                </Link>
-            ))}
-        </div>
-    );
-}
- 
+          {/* right */}
+          <div className="relative min-h-[82px] min-w-[120px]">
+            <Image
+              src={product.imageUrl}
+              alt={product.name}
+              fill
+              className="rounded-lg object-contain"
+            />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
 export default Products;
