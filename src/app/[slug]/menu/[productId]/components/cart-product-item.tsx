10e5bb27f5d11d39bde3a38f59f3c3ac8a64f411
Claudio OLiveira <dev.caon@gmail.com>
2025-02-26 23:14:33 -0300

    feat: add initial cart item component  (without functionality)

---
+++ src/app/[slug]/menu/[productId]/components/cart-product-item.tsx
@@ -0,0 +1,53 @@
+import { ChevronLeftIcon, ChevronRightIcon, Trash2Icon } from "lucide-react";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/helpers/format-currency";
+
+import { CartProduct } from "../../contexts/cart";
+
+interface CartItemProps {
+  product: CartProduct;
+}
+
+const CartProductItem = ({ product }: CartItemProps) => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-3">
+        {/* ESQUERDA */}
+        <div className="relative h-20 w-20 rounded-xl bg-gray-100/25">
+          <Image
+            src={product.imageUrl}
+            alt={product.name}
+            fill
+            className="object-contain"
+          />
+        </div>
+        <div className="space-y-1">
+          <p className="max-w-[90%] truncate text-ellipsis text-xs">
+            {product.name}
+          </p>
+          <p className="text-sm font-semibold">
+            {formatCurrency(product.price)}
+          </p>
+          {/* Quantidade */}
+          <div className="flex items-center gap-1 text-center">
+            <Button variant="outline" className="h-7 w-7 rounded-lg">
+              <ChevronLeftIcon />
+            </Button>
+            <p className="w-8 text-xs">{product.quantity}</p>
+            <Button variant="destructive" className="h-7 w-7 rounded-lg">
+              <ChevronRightIcon />
+            </Button>
+          </div>
+        </div>
+      </div>
+      {/* BOTÃO DELETAR */}
+      <Button variant="outline" className="h-7 w-7 rounded-lg">
+        <Trash2Icon />
+      </Button>
+    </div>
+  );
+};
+
+export default CartProductItem;
--- src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
@@ -8,19 +8,18 @@ import {
 } from "@/components/ui/sheet";
 
 import { CartContext } from "../../contexts/cart";
+import CartProductItem from "./cart-product-item";
 
 const CartSheet = () => {
   const { isOpen, toogleCart, products } = useContext(CartContext);
   return (
     <Sheet open={isOpen} onOpenChange={toogleCart}>
-      <SheetContent>
+      <SheetContent className="w-[85%] space-y-5">
         <SheetHeader>
-          <SheetTitle>Sacola</SheetTitle>
+          <SheetTitle className="text-left">Minha sacola</SheetTitle>
         </SheetHeader>
         {products.map((product) => (
-          <h1 key={product.id}>
-            {product.name} - {product.quantity}
-          </h1>
+          <CartProductItem product={product} key={product.id} />
         ))}
       </SheetContent>
     </Sheet>
